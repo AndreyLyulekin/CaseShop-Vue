4b65421a0c70dedbe55c8dfbdbfb4ac43f2b7236
AndreyLyulekin <andreilyulekin@yandex.ru>
2023-04-03 19:41:27 +1000

    trying to run gh-pages #2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  base: 'https://andreylyulekin.github.io/CaseShop-Vue/'
 })
